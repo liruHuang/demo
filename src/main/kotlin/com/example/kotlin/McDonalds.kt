0630e48be0a885d2e36c699972eae31e97c52560
liruHuang <ades246@gmail.com>
2024-12-20 11:00:45 +0800

    12/20 kotlin McDonalds ver2

--- src/main/kotlin/com/example/kotlin/McDonalds.kt
+++ src/main/kotlin/com/example/kotlin/McDonalds.kt
@@ -1,41 +1,47 @@
 package com.example.kotlin
 
-import java.io.*
-import java.util.function.Consumer
+import org.hibernate.boot.jaxb.internal.stax.BufferedXMLEventReader
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+class Item (val id: String, val name: String, val calories: Int, val price: Int) {
+    override fun toString(): String {
+        return "$id, $name, $calories, $price"
+    }
+}
+/*
 
 val menu = HashMap<String, Item>()
 class McDonalds {
-
     init {
-        val file = File("mcdonalds.txt")
 
-        val br = BufferedReader(FileReader(file))
+        val br = BufferedReader(FileReader("mcdonalds.txt"))
         var line = br.readLine()
         while (line != null) {
             val tokens = line.split(",")
-            val item = Item(tokens[0], tokens[1],tokens[2].toInt(),tokens[3].toInt())
+            val item = Item (tokens[0], tokens[1], tokens[2].toInt(), tokens[3].toInt())
             menu.put(item.id, item)
             line = br.readLine()
+
         }
     }
 
-    fun printMenu () {
+    fun printMenu(){
         menu.entries.forEach {
             println(it.value)
         }
     }
 }
-
+*/
 
 fun main() {
-    val McDonalds = McDonalds()
-    McDonalds.printMenu();
-}
+/*
+    val mcDonalds = McDonalds()
+    mcDonalds.printMenu()
+*/
 
-
-
-data class Item (val id : String, val name : String, val calorie : Int, val price : Int) {
-    override fun toString(): String {
-        return "$id, $name, $price, $id"
+    File("mcdonalds.txt").bufferedReader().lines().forEach {
+        println(it)
     }
 }
