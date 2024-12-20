849996fab2b49f3632469ef85d065993ebc3aec4
liruHuang <ades246@gmail.com>
2024-12-20 09:43:20 +0800

    12/20 practice

--- src/main/java/com/example/demo/McDonalds.java
+++ src/main/java/com/example/demo/McDonalds.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class McDonalds{
-    Map<String, Item> menu = new HashMap<>();
+public class McDonalds {
+    Map<Integer, Item> menu = new HashMap<>();
     public McDonalds(){
         File file = new File("mcdonalds.txt");
 
@@ -15,11 +15,11 @@ public class McDonalds{
             while (line != null) {
                 String[] tokens = line.split(",");
                 Item item = new Item(
-                        tokens[0],
+                        Integer.parseInt(tokens[0]),
                         tokens[1],
                         Integer.parseInt(tokens[2]),
                         Integer.parseInt(tokens[3]));
-                menu.put(item.id,item);
+                menu.put(item.getId(), item);
                 line = br.readLine();
             }
         } catch (IOException e) {
@@ -28,33 +28,68 @@ public class McDonalds{
     }
 
     public void printMenu(){
-        for (Map.Entry<String, Item> itemEntry : menu.entrySet()) {
+        for (Map.Entry<Integer, Item> itemEntry : menu.entrySet()) {
             System.out.println(itemEntry.getValue());
         }
     }
+
 
     public static void main(String[] args) {
         McDonalds mcdonalds = new McDonalds();
         mcdonalds.printMenu();
     }
 
+
 }
 
-class Item{
-    String id;
+
+class Item {
+    int id;
     String name;
-    int calorie;
+    int calories;
     int price;
 
     @Override
     public String toString() {
-        return id + "\t" + name + "\t" + calorie + "\t" + price;
+        return id+","+name+","+calories+","+price;
     }
 
-    public Item(String id, String name, int calorie, int price) {
+    public Item(int id, String name, int calories, int price) {
         this.id = id;
         this.name = name;
-        this.calorie = calorie;
+        this.calories = calories;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
         this.price = price;
     }
 }
--- src/main/java/com/example/demo/netWork/Tester.java
+++ src/main/java/com/example/demo/netWork/Tester.java
@@ -1,6 +1,10 @@
 package com.example.demo.netWork;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,20 +15,20 @@ public class Tester {
             URL url = new URL("https://odws.hccg.gov.tw/001/Upload/25/opendataback/9059/27/385f68c2-95e6-48cd-a9f8-0d3ffa35554b.json");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             InputStream is = connection.getInputStream();
-            BufferedReader br= new BufferedReader(new InputStreamReader(is));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
             String line = br.readLine();
             StringBuffer json = new StringBuffer();
-            while(line != null){
-                json.append(line);
+            while (line != null) {
                 line = br.readLine();
+                json.append(line);
             }
             System.out.println(json.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
